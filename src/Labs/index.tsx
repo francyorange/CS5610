276208f57d7fc58497eb4ab4eedcf7b8f0097d33
Yanling Peng <108501928+francyorange@users.noreply.github.com>
2024-10-30 05:05:56 +0000

    finish 2.3

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,16 +3,18 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
 export default function Labs() {
     return (
         <div>
             <h1>Labs by Yanling Peng (Section 3)</h1>
             <TOC />
             <Routes>
-                <Route path="/" element={<Navigate to="Lab3" />} />
+                <Route path="/" element={<Navigate to="Lab4" />} />
                 <Route path="Lab1" element={<Lab1 />} />
                 <Route path="Lab2" element={<Lab2 />} />
                 <Route path="Lab3/*" element={<Lab3 />} />
+                <Route path="Lab4/*" element={<Lab4 />} />
             </Routes>
         </div>
     );
