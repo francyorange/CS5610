71fdf09241c88ce9b770708b398f60a1093a4cb8
Yanling Peng <108501928+francyorange@users.noreply.github.com>
2024-11-21 07:36:31 +0000

    finish 3.6

--- kanbas-node-server-app/Lab5/WorkingWithArrays.js
+++ kanbas-node-server-app/Lab5/WorkingWithArrays.js
@@ -14,11 +14,6 @@ export default function WorkingWithArrays(app) {
         }
         res.json(todos);
     });
-    app.get("/lab5/todos/:id", (req, res) => {
-        const { id } = req.params;
-        const todo = todos.find((t) => t.id === parseInt(id));
-        res.json(todo);
-    });
     app.get("/lab5/todos/create", (req, res) => {
         const newTodo = {
             id: new Date().getTime(),
@@ -28,18 +23,57 @@ export default function WorkingWithArrays(app) {
         todos.push(newTodo);
         res.json(todos);
     });
+    app.post("/lab5/todos", (req, res) => {
+        const newTodo = { ...req.body, id: new Date().getTime() };
+        todos.push(newTodo);
+        res.json(newTodo);
+    });
+
+    app.get("/lab5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        res.json(todo);
+    });
+
     app.get("/lab5/todos/:id/delete", (req, res) => {
         const { id } = req.params;
         const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
         todos.splice(todoIndex, 1);
         res.json(todos);
     });
+    app.delete("/lab5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        if (todoIndex === -1) {
+            res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
+            return;
+        }
+        todos.splice(todoIndex, 1);
+        res.sendStatus(200);
+    });
+
     app.get("/lab5/todos/:id/title/:title", (req, res) => {
         const { id, title } = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
         todo.title = title;
         res.json(todos);
     });
+    app.put("/lab5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        if (todoIndex === -1) {
+            res.status(404).json({ message: `Unable to update Todo with ID ${id}` });
+            return;
+        }
+        todos = todos.map((t) => {
+            if (t.id === parseInt(id)) {
+                return { ...t, ...req.body };
+            }
+            return t;
+        });
+        res.sendStatus(200);
+    });
+
     app.get("/lab5/todos/:id/completed/:completed", (req, res) => {
         const { id, completed } = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
